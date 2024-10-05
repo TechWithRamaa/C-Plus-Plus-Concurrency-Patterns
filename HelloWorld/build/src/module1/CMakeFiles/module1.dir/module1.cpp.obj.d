c77ca5c7b4065625eb0e0a185e6640d7c532c0cd
Ramaa <ramadhevi.rk@oracle.com>
2024-10-05 23:58:55 +0530

    Remove build folder from tracking.

--- HelloWorld/build/src/module1/CMakeFiles/module1.dir/module1.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-src/module1/CMakeFiles/module1.dir/module1.cpp.obj: \
- E:/cpp-workspace/TechWithRamaa/HelloWorld/src/module1/module1.cpp
--- HelloWorld/build/src/module2/CMakeFiles/module2.dir/module2.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-src/module2/CMakeFiles/module2.dir/module2.cpp.obj: \
- E:/cpp-workspace/TechWithRamaa/HelloWorld/src/module2/module2.cpp
